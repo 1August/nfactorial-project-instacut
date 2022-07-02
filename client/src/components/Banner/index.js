08ec25631a676c48dd50daf6c8965d7978ac601a
1August <boktaban@gmail.com>
2022-07-02 18:20:51 +0600

    Add icon, project description

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -7,14 +7,13 @@ export const Banner = props => {
                 <div className="container">
                     <div className="banner__info">
                         <h1>Automate creating your Instagram post.</h1>
-                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi cumque iste itaque modi nemo odio quisquam ratione saepe veritatis.</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequatur, delectus
+                        <h4>Easy tool to create fast post with dividing text and pictures.</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequatur, delectus
                             exercitationem expedita explicabo ipsa ipsam libero molestias neque, odio optio quam quibusdam
-                            ratione. Accusantium excepturi fuga illum officia saepe.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias debitis explicabo nemo pariatur.
-                            Ad earum facilis voluptatibus? Aperiam architecto, autem beatae corporis expedita in iusto
-                            laudantium magni molestias mollitia necessitatibus nisi numquam odio officiis quaerat quasi
-                            quidem rem temporibus ullam!</p>
+                            ratione. Accusantium excepturi fuga illum officia saepe.
+                        </p>
+                        <p className={'banner__helper'}>Click <span>+</span> icon to start</p>
                     </div>
                 </div>
             </div>
